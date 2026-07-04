d22d637953254dee53a0484c006c3831a11b7557
sheikhnadimjahan <sheikhnadim@live.com>
2026-07-05 04:32:06 +0600

    Create populateAuthors.ts

---
+++ src/collections/Articles/populateAuthors.ts
@@ -0,0 +1,12 @@
+import { FieldHook } from 'payload'
+
+const populateAuthors: FieldHook = async ({ value, req }) => {
+  if (!value) return value
+  const users = await req.payload.find({
+    collection: 'users',
+    where: { id: { in: value } },
+  })
+  return users.docs
+}
+
+export default populateAuthors
