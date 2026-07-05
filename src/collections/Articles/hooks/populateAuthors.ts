db1634b79d394ecee945c55e58456ac7474680f9
thandabarry-pixel <thandabarry@gmail.com>
2026-07-05 10:06:31 +0600

    Remove populateAuthors hook from Articles

--- src/collections/Articles/hooks/populateAuthors.ts
+++ src/collections/Articles/hooks/populateAuthors.ts
@@ -1,10 +1,1 @@
-import { FieldHook } from 'payload'
 
-export const populateAuthors: FieldHook = async ({ value, req }) => {
-  if (!value) return value
-  const users = await req.payload.find({
-    collection: 'users',
-    where: { id: { in: value } },
-  })
-  return users.docs
-}
