2ac6bdc0d45affb6feffd6dff4c918b68168bf5c
thandabarry-pixel <thandabarry@gmail.com>
2026-07-05 10:06:08 +0600

    Remove revalidation hooks for article changes

--- src/collections/Articles/hooks/revalidateArticle.ts
+++ src/collections/Articles/hooks/revalidateArticle.ts
@@ -1,19 +1,1 @@
-import { CollectionAfterChangeHook, CollectionAfterDeleteHook } from 'payload'
-import { revalidatePath } from 'next/cache'
 
-const runRevalidate = async (doc: any) => {
-  try {
-    await revalidatePath(`/articles/${doc.id}`)
-    await revalidatePath(`/`)
-  } catch (err) {
-    console.error('Revalidation failed', err)
-  }
-}
-
-export const revalidateArticle: CollectionAfterChangeHook = async ({ doc }) => {
-  await runRevalidate(doc)
-}
-
-export const revalidateDelete: CollectionAfterDeleteHook = async ({ doc }) => {
-  await runRevalidate(doc)
-}
